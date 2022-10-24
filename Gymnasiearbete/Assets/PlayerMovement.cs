db3527bea19cb458f81cb4828a675d32c69e5542
urmomurmum <hannah.edlund@hotmail.se>
2022-10-24 10:29:03 +0200

    test

--- Gymnasiearbete/Assets/PlayerMovement.cs
+++ Gymnasiearbete/Assets/PlayerMovement.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
 
     // Update is called once per frame
     void Update()
