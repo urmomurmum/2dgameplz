1adca8ba0fc7d5366305c2b716edde11de9b83d4
swedishAPACHE <emile.almroth@gmail.com>
2022-10-24 12:44:24 +0200

    v.0.1 (Laser destroy fix)

--- Gymnasiearbete/Assets/Scripts/ShootLaser.cs
+++ Gymnasiearbete/Assets/Scripts/ShootLaser.cs
@@ -9,6 +9,7 @@ public class ShootLaser : MonoBehaviour
 
     void Update()
     {
+        Destroy(GameObject.Find("Laser Beam"));
         beam = new LaserBeam(gameObject.transform.position, gameObject.transform.right , material);
     }
 }
