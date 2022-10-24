752b2b9d738b485bff31ed4f2e9ae1e680dbac6a
urmomurmum <hannah.edlund@hotmail.se>
2022-10-24 11:22:17 +0200

    movement v. 0.2

---
+++ Gymnasiearbete/Assets/Scripts/BPlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BPlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input
+
+        movement.x = Input.GetAxisRaw("Horizontal2");
+        movement.y = Input.GetAxisRaw("Vertical2");
+    }
+
+    private void FixedUpdate()
+    {
+        //Movement
+
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
